cb76f293004065052391a8f4ec386858d160ba1c
Mirza Moiz Baig <mirzab@nait.ca>
2026-02-25 20:39:23 -0700

    looping and exceptions end added

--- exercises_with_loops_and_exceptions_end/average_age_of_students.py
+++ exercises_with_loops_and_exceptions_end/average_age_of_students.py
@@ -5,6 +5,14 @@ while True:
     age = input("Enter the age of a student or 'stop' to finish:")
     if age == "stop":
         break
-    age_count += int(age)
-    student_count += 1
-print(f"The average age of the students is  {age_count/student_count}")
+    else: 
+        try:
+            age_count += int(age)
+            student_count += 1
+        except ValueError:
+            print("Please enter a number or 'stop' to finish")
+try:
+    print(f"The average age of the students is  {age_count/student_count}")
+except ZeroDivisionError:
+    print("You didn't enter any ages")
+
